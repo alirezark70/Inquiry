21e2947853ef1237afde5836420cd3259bb2fa84
alirezark70 <rezaee.alireza7098@gmail.com>
2025-08-28 18:51:04 +0330

    Add AuthorizationBehaviour handle in application Service

---
+++ Inquiry/1.Core/ApplicationService/Inquiry.Core.ApplicationService/Behaviors/AuthorizationBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inquiry.Core.ApplicationService.Behaviors
+{
+    public class AuthorizationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Inquiry/1.Core/ApplicationService/Inquiry.Core.ApplicationService/obj/Debug/net9.0/Inquiry.Core.ApplicationService.AssemblyInfo.cs
+++ Inquiry/1.Core/ApplicationService/Inquiry.Core.ApplicationService/obj/Debug/net9.0/Inquiry.Core.ApplicationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inquiry.Core.ApplicationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e942e4b23a881b2b8cd015da26d0f71547a7e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd08c1c3f329efe8a34a9ecd0ae1343c5dc4197c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inquiry.Core.ApplicationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inquiry.Core.ApplicationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
